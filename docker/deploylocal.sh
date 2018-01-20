1b7e5190781f87bcee052699babfd006a6e08b06
马晓辉 <115504218@qq.com>
2018-01-21 02:51:26 +0800

    调整了redocker.sh

--- docker/deploylocal.sh
+++ docker/deploylocal.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#!chmod +x ./deploy.sh
+#!chmod +x ./deploylocal.sh
 
 if [ "$#" != "1" ]; then
     echo "usage: $0 sit | uat"
@@ -7,31 +7,45 @@ if [ "$#" != "1" ]; then
 fi
 
 BASE_PATH=/Users/mark/work/git/spclod/spcloud
+BASE_PATH_PJ=/Users/mark/work/git/spclod/spcloud/$1
 REMOTE_PATH_ROOT=/home/parallels/docker/spcloud
 REMOTE_PATH_PJ=$REMOTE_PATH_ROOT/$1
 REMOTE_PATH=parallels@10.211.55.5:$REMOTE_PATH_PJ
 
+PWD=mxhzmm123!@#
+USER=parallels
+IP=10.211.55.5
 
 echo web update
 cd $BASE_PATH
 
-if [ "$1" = "eureka-server" ]; then
-    echo rm $1 and mkdir $1
-    sshpass -p "mxhzmm123!@#" ssh -p 22 parallels@10.211.55.5 "cd $REMOTE_PATH_ROOT ;rm -r -f $1 ; mkdir $1"
+#if [ "$1" = "eureka-server" ]; then
+#fi
 
-    echo send jar
-    sshpass -p "mxhzmm123!@#" scp -P 22 $BASE_PATH/eureka-server/target/eureka-server-0.0.1-SNAPSHOT.jar $REMOTE_PATH/eureka-server-0.0.1-SNAPSHOT.jar
+echo rm $1 and mkdir $1
+sshpass -p "mxhzmm123!@#" ssh -p 22 parallels@10.211.55.5 "cd $REMOTE_PATH_ROOT ;rm -r -f $1 ; mkdir $1"
 
-    echo send dockerfile
-    sshpass -p "mxhzmm123!@#" scp -P 22 $BASE_PATH/eureka-server/Dockerfile $REMOTE_PATH/Dockerfile
+echo send jar
+sshpass -p "mxhzmm123!@#" scp -P 22 $BASE_PATH_PJ/target/$1-0.0.1-SNAPSHOT.jar $REMOTE_PATH/$1-0.0.1-SNAPSHOT.jar
 
-    echo delete image
-    sshpass -p "mxhzmm123!@#" ssh -p 22 parallels@10.211.55.5 " docker rm -f $(docker ps -f name=$1 -q) "
-    sshpass -p "mxhzmm123!@#" ssh -p 22 parallels@10.211.55.5 " docker rmi $1 "
+echo send dockerfile
+sshpass -p "mxhzmm123!@#" scp -P 22 $BASE_PATH_PJ/Dockerfile $REMOTE_PATH/Dockerfile
 
-    echo build image
-    sshpass -p "mxhzmm123!@#" ssh -p 22 parallels@10.211.55.5 " cd $REMOTE_PATH_PJ ; docker build -t $1:latest . "
-    sshpass -p "mxhzmm123!@#" ssh -p 22 parallels@10.211.55.5 " docker run --name $1 -d -p 8761:8761 $1:latest "
-fi
+echo create redocker.sh
+cd $BASE_PATH_PJ
+rm redocker.sh
+echo docker rm -f '$('docker ps -f name=$1 -q')' >> redocker.sh
+echo docker rmi $1 >> redocker.sh
+echo docker build -t $1:latest . >> redocker.sh
+echo docker run --name $1 -d -p 8761:8761 $1:latest >> redocker.sh
+
+echo send redocker.sh
+sshpass -p "mxhzmm123!@#" scp -P 22 $BASE_PATH_PJ/redocker.sh $REMOTE_PATH/redocker.sh
+
+echo set promise redocker.sh
+sshpass -p "mxhzmm123!@#" ssh -p 22  parallels@10.211.55.5 "cd $REMOTE_PATH_PJ ; chmod +x ./redocker.sh"
+
+echo exec redocker.sh
+sshpass -p "mxhzmm123!@#" ssh -p 22  parallels@10.211.55.5 "cd $REMOTE_PATH_PJ ; ./redocker.sh"
 
 echo $1 ok.
